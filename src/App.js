3a9b4797561f69dbd9e45405f05256ef63a52a0e
Heather Herries <hherries@yahoo.com>
2019-02-19 21:27:39 -0700

    Assignment Routers: completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album}
         </main>
       </div>
     );
