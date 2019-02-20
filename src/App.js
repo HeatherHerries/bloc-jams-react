3c117bd10130b7fbf396e7e0f8120fa337cdb0d9
Heather Herries <hherries@yahoo.com>
2019-02-19 20:45:00 -0700

    Checkpoint Routing: completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/library'>Library</Link>
+          </nav>
+          <h1>Bloc Jams</h1>
         </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+        </main>
       </div>
     );
   }
---
+++ src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Library = () => (
+    <section className="library">
+        Library will go here
+    </section>
+);
+
+export default Library;
