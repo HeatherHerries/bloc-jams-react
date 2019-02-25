f3e06b70fa6961bddfad11c75ea79675c00a6a7d
Heather Herries <hherries@yahoo.com>
2019-02-25 15:19:13 -0700

    Checkpoint PlayerBar: Buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
@@ -43,6 +44,14 @@ class Album extends Component {
             if (!isSameSong) { this.setSong(song); }
             this.play();
         }
+    }
+
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
     }
 
     onHover(index) {
@@ -91,6 +100,12 @@ class Album extends Component {
                             </tr>)}
                     </tbody>
                 </table>
+                <PlayerBar 
+                    isPlaying={this.state.isPlaying}
+                    currentSong={this.state.currentSong}
+                    handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                    handlePrevClick={() => this.handlePrevClick()}
+                />
             </section>
         );
     }
---
+++ src/components/PlayerBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class PlayerBar extends Component {
+    render() {
+        return (
+            <section className="player-bar">
+                <section id="bottons">
+                    <button id="previous" onClick={this.props.handlePrevClick}>
+                        <span className="icon ion-md-skip-backward"></span>
+                    </button>
+                    <button id="play-pause" onClick={this.props.handleSongClick} >
+                        <span className={this.props.isPlaying ? 'icon ion-md-pause' : 'icon ion-md-play'}></span>
+                    </button>
+                    <button id="next">
+                        <span className="icon ion-md-skip-forward"></span>
+                    </button>
+                </section>
+                <section id="time-control">
+                    <div className="current-time">-:--</div>
+                    <input type="range" className="seek-bar" value="0" />
+                    <div className="total-time">-:--</div>
+                </section>
+                <section id="volume-control">
+                    <div className="icon ion-md-volume-low"></div>
+                    <input type="range" className="seek-bar" value="80" />
+                    <div className="icon ion-md-volume-high"></div>
+                </section>
+            </section>
+        )
+    }
+}
+
+export default PlayerBar;
