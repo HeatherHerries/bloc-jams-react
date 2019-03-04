469e407edcf70061047ccdae5ab274804925c8b8
Heather Herries <hherries@yahoo.com>
2019-03-03 19:14:43 -0700

    Assignment Player Bar: Range Inputs: completed

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -16,7 +16,7 @@ class PlayerBar extends Component {
                     </button>
                 </section>
                 <section id="time-control">
-                    <div className="current-time">{this.props.currentTime}</div>
+                    <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
                     <input
                         type="range"
                         className="seek-bar"
@@ -26,12 +26,19 @@ class PlayerBar extends Component {
                         step="0.01"
                         onChange={this.props.handleTimeChange}
                     />
-                    <div className="total-time">{this.props.duration}</div>
+                    <div className="total-time">{this.props.formatTime(this.props.duration)}</div>
                 </section>
                 <section id="volume-control">
-                    <div className="icon ion-md-volume-low"></div>
-                    <input type="range" className="seek-bar" value="80" />
-                    <div className="icon ion-md-volume-high"></div>
+                    <div className="volume">{this.props.volume}</div>
+                    <input
+                        type="range"
+                        className="seek-bar"
+                        value={(this.props.volume) || 80}
+                        max="1"
+                        min="0"
+                        step="0.01"
+                        onChange={this.props.handleVolumeChange}
+                />
                 </section>
             </section>
         )
